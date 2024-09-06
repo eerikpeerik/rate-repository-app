d3b82a765f84ddf565025d9df59471d78e8e54f3
eerikpeerik <erik.astrom@abo.fi>
2024-09-06 11:56:17 +0300

    Idk what to write ok

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,17 @@
+import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    // ...
+    backgroundColor: 'darkturquoise'
+  },
+  // ...
+});
+
+const AppBar = () => {
+  return <View style={styles.container}>{'Repositories'}</View>;
+};
+
+export default AppBar;
