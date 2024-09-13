b064410272cfcd4866a783a84cf216a622403b04
eerikpeerik <erik.astrom@abo.fi>
2024-09-13 12:57:17 +0300

    Completed 10.4

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,25 +2,41 @@ import { View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import Text from './Text';
 import theme from '../theme';
+import { Pressable } from 'react-native';
 
 const styles = StyleSheet.create({
+
   container: {
-    paddingTop: Constants.statusBarHeight,
+    paddingTop: Constants.statusBarHeight + 5,
     // ...
-    backgroundColor: theme.colors.textPrimary
+
+  backgroundColor: theme.colors.textPrimary,
+    padding: 5
+    
   },
-  text: {
-    color: 'white'
-  }
+text: {
+  color: 'white'
+}
   // ...
 
 });
 
+
+
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Repository</Text>
+      <Pressable onPress={onPressFunction}>
+        
+        <Text style={styles.text}>Repository</Text>
+      </Pressable>
+      
+
     </View>);
 };
+
+const onPressFunction = () => {
+  console.log("Pressed")
+}
 
 export default AppBar;
